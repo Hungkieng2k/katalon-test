a52c947a8c4cff3950822039f49aa27a17b2df44
HUNG <hung.kieng2k@gmail.com>
2023-04-27 16:19:43 +0700

    a

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>t1</name>
+   <name>New Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,9 +10,9 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>f942e0a9-b969-4ee1-9454-ba90eea81928</testSuiteGuid>
+   <testSuiteGuid>c4626a2b-d340-435b-b6bd-cb97560258b3</testSuiteGuid>
    <testCaseLink>
-      <guid>d61bb352-0d98-47fe-94d6-2a489c46785a</guid>
+      <guid>522d19f9-522b-443c-9f56-6d0b4d8b3a44</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Apointment/login</testCaseId>
